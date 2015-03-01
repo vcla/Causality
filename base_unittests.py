56dec8108a48bb7b7cbfc5c5b33f0250e5d83b93
Amy <amy@tentacle.net>
2015-03-01 10:39:23 -0800

    added tests, changed parse times and grammar probabilities

--- base_unittests.py
+++ base_unittests.py
@@ -1,15 +1,20 @@
 import unittest
 import causal_grammar
 import xml.etree.ElementTree as ET
+import dealWithDBResults
+
+kDebug = True
 
 fluents_simple_light = {
-	12:  { "light": .7},
-	14:  { "light": .01},
+	6:  { "light": causal_grammar.probability_to_energy(.6)}, #light turns on at 6
+	8:  { "light": causal_grammar.probability_to_energy(.9)}, #light turns on at 8
 }
 
 actions_simple_light = {
-	10:  { "E1_START": {"energy": .1, "agent": ("uuid4")} },
+	5:  { "E1_START": {"energy": causal_grammar.probability_to_energy(.9), "agent": ("uuid4")} },
 }
+
+### desired solution: light off to start.  then "turned on" at 5 for "on" at 8.
 
 # our causal forest:
 causal_forest_light = [
@@ -18,11 +23,11 @@ causal_forest_light = [
 	"symbol_type": "fluent",
 	"symbol": "light_on",
 	"children": [
-		{ "node_type": "and", "probability": .3, "children": [
+		{ "node_type": "and", "probability": .6, "children": [ #on inertially -- higher chance of occurrence?
 				{ "node_type": "leaf", "symbol": "light_on", "symbol_type": "prev_fluent" },
 				{ "node_type": "leaf", "symbol": "E1_START", "symbol_type": "nonevent", "timeout": 10 },
 		]},
-		{ "node_type": "and", "probability": .7, "children": [
+		{ "node_type": "and", "probability": .4, "children": [ #on by causing action E1_START
 				{ "node_type": "leaf", "symbol_type": "prev_fluent", "symbol": "light_off" },
 				{ "node_type": "leaf", "symbol_type": "event", "symbol": "E1_START", "timeout": 10 },
 			]
@@ -33,11 +38,11 @@ causal_forest_light = [
 	"symbol_type": "fluent",
 	"symbol": "light_off",
 	"children": [
-		{ "node_type": "and", "probability": .3, "children": [
+		{ "node_type": "and", "probability": .6, "children": [ #off inertially
 				{ "node_type": "leaf", "symbol": "light_off", "symbol_type": "prev_fluent" },
 				{ "node_type": "leaf", "symbol": "E1_START", "symbol_type": "nonevent", "timeout": 10 },
 		]},
-		{ "node_type": "and", "probability": .7, "children": [
+		{ "node_type": "and", "probability": .4, "children": [ #off by causing action E1_START
 				{ "node_type": "leaf", "symbol_type": "prev_fluent", "symbol": "light_on" },
 				{ "node_type": "leaf", "symbol_type": "event", "symbol": "E1_START", "timeout": 10 },
 			]
@@ -46,8 +51,9 @@ causal_forest_light = [
 },
 ]
 
-xml_string = causal_grammar.process_events_and_fluents(causal_forest_light, fluents_simple_light, actions_simple_light, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,True) # final True: suppress output
+xml_string = causal_grammar.process_events_and_fluents(causal_forest_light, fluents_simple_light, actions_simple_light, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug) # !kDebug: suppress output
 root = ET.fromstring(xml_string)
+print(xml_string)
 
 class LightingTestCase(unittest.TestCase):
 
@@ -62,7 +68,20 @@ class LightingTestCase(unittest.TestCase):
 
 	def testZeroState(self):
 		light_0 = root.findall("./fluent_changes/fluent_change[@frame='0']")
-		assert light_0[0].attrib['new_value'] == 'on', "should have decided light started out as on; was: {}".format(light_0[0].attrib['new_value'])
+		assert light_0[0].attrib['new_value'] == 'off', "should have decided light started out as off; was: {}".format(light_0[0].attrib['new_value'])
+
+	def testTrueFluentChangeState(self):
+		light_8 = root.findall("./fluent_changes/fluent_change[@frame='8']")
+		assert light_8[0].attrib['new_value'] == 'on', "should have decided light changed to on at 8; was: {}".format(light_8[0].attrib['new_value'])
+
+	def testMisdetectedFluentChangeState(self):
+		light_6 = root.findall("./fluent_changes/fluent_change[@frame='6']")
+		assert (not light_6), "should have shown no change at 6; changed to: {}".format(light_6[0].attrib['new_value'])
+
+	def testForActionTooEarly(self):
+		#queryXMLForActionBetweenFrames(xml,action,frame1,frame2)
+		action_occurrences = dealWithDBResults.queryXMLForActionBetweenFrames(xml_string,"E1_START",0,10)
+		assert (not action_occurrences), "should have had no action before 7; n times action occurred: {}".format(action_occurrences)
 
 	def testBetterAction(self):
 		"""Test case A. note that all test method names must begin with 'test.'"""
