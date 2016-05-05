1df8cf724f1eb51cca110f3eeeee27c3a754cd8b
Amy Fire <amy@tentacle.net>
2016-05-05 07:18:42 -0700

    a few more unit tests in base_unittests.py over the last month or so

--- base_unittests.py
+++ base_unittests.py
@@ -40,6 +40,70 @@ causal_forest_light = [
 },
 ]
 
+# the ideal result: takes the first action because it has lower energy
+class LightingTestCaseFirstOfTwoActions(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		fluents_simple_light = {
+			6:  { "light": causal_grammar.probability_to_energy(.6)}, #light turns on at 6 -- energy = .51
+		}
+		actions_simple_light = {
+			4:  { "FLIPSWITCH": {"energy": causal_grammar.probability_to_energy(.9), "agent": ("uuid4")} }, #energy = .11
+			5:  { "FLIPSWITCH": {"energy": causal_grammar.probability_to_energy(.6), "agent": ("uuid4")} }, #energy = .51
+		}
+		xml_string = causal_grammar.process_events_and_fluents(causal_forest_light, fluents_simple_light, actions_simple_light, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = True) # !kDebug: suppress output
+		cls.root = ET.fromstring(xml_string)
+		cls.xml_string = xml_string
+		if kDebug:
+			print(xml_string)
+
+	def testAction(self):
+		# known 'failure': we can't trust just getting the event at frame 4 because we play around with when we want the event to actually happen (for instance, causing it to happen when the fluent happens)
+		#action = self.root.findall("./actions/event[@frame='4']")
+		#assert action, "should have found action at frame 4; instead got {}".format(self.xml_string)
+		#TODO: we don't xml out the individual energies, just the whole energy, so we're going to leave this a little fragile for now
+		#energy = causal_grammar.probability_to_energy(.9)
+		energy = 19.365
+		action = self.root.findall("./actions/event")
+		assert action, "did not complete with /any/ action"
+		assert len(action) == 1, "found multiple actions ~ {}".format(cls.xml_string)
+		action = action[0]
+		assert action.attrib['action'] == 'FLIPSWITCH', "found wrong action, how did that happen!? ~ {}".format(cls.xml_string)
+		assert abs(float(action.attrib['energy'])-energy) < .000001, "wrong energy ~ {} != expected {}".format(action.attrib['energy'],energy)
+
+# the ideal result: takes the second action because it has lower energy
+class LightingTestCaseSecondOfTwoActions(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		fluents_simple_light = {
+			6:  { "light": causal_grammar.probability_to_energy(.6)}, #light turns on at 6 -- energy = .51
+		}
+		actions_simple_light = {
+			4:  { "FLIPSWITCH": {"energy": causal_grammar.probability_to_energy(.6), "agent": ("uuid4")} }, #energy = .51
+			5:  { "FLIPSWITCH": {"energy": causal_grammar.probability_to_energy(.9), "agent": ("uuid4")} }, #energy = .11
+		}
+		xml_string = causal_grammar.process_events_and_fluents(causal_forest_light, fluents_simple_light, actions_simple_light, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = True) # !kDebug: suppress output
+		cls.root = ET.fromstring(xml_string)
+		cls.xml_string = xml_string
+		if kDebug:
+			print(xml_string)
+
+	def testAction(self):
+		# known failure: we can't trust just getting the event at frame 5 because we play around with when we want the event to actually happen (for instance, causing it to happen when the fluent happens)
+		#action = self.root.findall("./actions/event[@frame='5']")
+		#assert action, "should have found action at frame 5; instead got {}".format(self.xml_string)
+		#TODO: we don't xml out the individual energies, just the whole energy, so we're going to leave this a little fragile for now
+		#energy = causal_grammar.probability_to_energy(.9)
+		energy = 19.365
+		action = self.root.findall("./actions/event")
+		assert action, "did not complete with /any/ action"
+		assert len(action) == 1, "found multiple actions ~ {}".format(cls.xml_string)
+		action = action[0]
+		assert action.attrib['action'] == 'FLIPSWITCH', "found wrong action, how did that happen!? ~ {}".format(cls.xml_string)
+		assert abs(float(action.attrib['energy'])-energy) < .000001, "wrong energy ~ {} != expected {}".format(action.attrib['energy'],energy)
+
 
 ######################## TEST CLASS: 2 FLUENTS, 2nd IS CORRECT ###################
 # the ideal result: action at 5, light changes to on at 8
@@ -340,18 +404,96 @@ class LightingTestCase2ndOf2Actions(unittest.TestCase):
 		action_occurrences += xml_stuff.queryXMLForActionBetweenFrames(self.root,"A4",0,15)
 		assert (not action_occurrences), "should not have had action FLIPSWITCH, A3, A4; n times action occurred: {}".format(action_occurrences)
 
+######################## NEW TEST CLASS: TESTING NONEVENT
+class NoneventTimeouts(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		causal_forest = [
+		{
+			"node_type": "root",
+			"symbol_type": "fluent",
+			"symbol": "fluent_on",
+			"children": [
+				{ "node_type": "and", "probability": .6, "children": [ #on inertially - not turned off
+						{ "node_type": "leaf", "symbol": "fluent_on", "symbol_type": "prev_fluent" },
+						{ "node_type": "leaf", "symbol": "OFFACTION", "symbol_type": "nonevent", "timeout": 3 },
+				]},
+				{ "node_type": "and", "probability": .4, "children": [ #on by causing action
+						{ "node_type": "leaf", "symbol_type": "prev_fluent", "symbol": "fluent_off" },
+						{ "node_type": "leaf", "symbol_type": "event", "symbol": "ONACTION", "timeout": 1 },
+					]
+				},
+			],
+		}, {
+			"node_type": "root",
+			"symbol_type": "fluent",
+			"symbol": "fluent_off",
+			"children": [
+				{ "node_type": "and", "probability": .6, "children": [ #off inertially #energy = .51
+						{ "node_type": "leaf", "symbol": "fluent_off", "symbol_type": "prev_fluent" },
+						{ "node_type": "leaf", "symbol": "OFFACTION", "symbol_type": "nonevent", "timeout": 3 },
+				]},
+				{ "node_type": "and", "probability": .4, "children": [ #off by causing action A1
+						{ "node_type": "leaf", "symbol_type": "prev_fluent", "symbol": "fluent_on" },
+						{ "node_type": "leaf", "symbol_type": "event", "symbol": "OFFACTION", "timeout": 1 },
+					]
+				},
+			],
+		},
+		]
+		fluents_simple = {
+			5:  { "fluent": causal_grammar.probability_to_energy(.9)}, #light turns off at 5 -- energy = .51
+		}
+		actions_simple = {
+			1:  { "OFFACTION": {"energy": causal_grammar.probability_to_energy(.9), "agent": ("uuid4")} }, #energy = .11
+		}
+		xml_string = causal_grammar.process_events_and_fluents(causal_forest, fluents_simple, actions_simple, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = True) # !kDebug: suppress output
+		cls.root = ET.fromstring(xml_string)
+		if kDebug:
+			print(xml_string)
+
+	def testFluentNonEvent(self):
+		assert 0
+
 ######################## NEW TEST CLASS: TWO ACTIONS, FIRST GETS LOST
 # the ideal result:  action 
 class TrampledMonitorAction(unittest.TestCase):
 
 	@classmethod
 	def setUpClass(cls):
-		import causal_grammar_summerdata
-		causal_forest = []
-		for root in causal_grammar_summerdata.causal_forest:
-			if root['symbol'].startswith('screen' + "_"):
-				causal_forest.append(root)
-		causal_forest_modified = causal_forest
+		causal_forest_abbreviated = [ #NOTE: this was yanked from causal_grammar_summerdata.py on 2016-04-24
+			("root", "fluent", "screen_on", .5, False, [ #SCREEN ON
+				("and", False, False, .35, False, [ #ON INERTIALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_END", False, 1, False), ]
+				),
+				("and", False, False, .3, False, [ #ON CAUSALLY
+					("leaf", "prev_fluent", "screen_off", False, False, False),
+					("leaf", "event", "usecomputer_START", False, 100, False), ]
+				),
+				("and", False, False, .35, False, [ #ON CONTINUOUSLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_START", False, 100, False), ]
+				), ]
+			),
+			("root", "fluent", "screen_off", .5, False, [ #SCREEN OFF
+				("and", False, False, .3, False, [ #OFF CAUSALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_END", False, 30, False) ]
+				),
+				("and", False, False, .4, False, [ # OFF INERTIALLY - due to the screensaver kicking on (so causally changed because of non-action)
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 100, False),
+					("leaf", "nonevent", "usecomputer_END", False, 1000, False) ]
+				),
+				("and", False, False, .3, False, [ # OFF INERTIALLY - no change because didn't start using the computer
+					("leaf", "prev_fluent", "screen_off", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 200, False) ]
+				) ]
+			)
+		]
+		causal_forest_modified = causal_grammar.generate_causal_forest_from_abbreviated_forest(causal_forest_abbreviated)
 		fluents = {
 			550:  { "screen": 0.969968},
 		}
@@ -364,12 +506,137 @@ class TrampledMonitorAction(unittest.TestCase):
 		}
 		xml_string = causal_grammar.process_events_and_fluents(causal_forest_modified, fluents, actions, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = True) # !kDebug: suppress output
 		cls.root = ET.fromstring(xml_string)
+		cls.xml_string = xml_string
 		if kDebug:
 			print(xml_string)
 
 	def testCorrectAction(self):
-		action_occurrences = xml_stuff.queryXMLForActionBetweenFrames(self.root,"usecomputer_END",528,559)
-		assert (action_occurrences == 1), "should have had action usecomputer_END; n times action occurred: {}".format(action_occurrences)
+		start = 527
+		end = 551
+		action_occurrences = xml_stuff.queryXMLForActionBetweenFrames(self.root,"usecomputer_END",start,end)
+		assert (action_occurrences == 1), "should have had action usecomputer_END between {} and {}; n times action occurred: {} [{}]".format(start,end,action_occurrences,self.xml_string)
+
+
+######################## NEW TEST CLASS: Two timeouts for a single action. First fluent is answer: so person turned off computer.
+class MutlipleTimeoutsShorterTimeoutWins(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		causal_forest_abbreviated = [ #NOTE: this was yanked from causal_grammar_summerdata.py on 2016-04-24
+			("root", "fluent", "screen_on", .5, False, [ #SCREEN ON
+				("and", False, False, .4, False, [ #ON INERTIALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_END", False, 1, False), ]
+				),
+				("and", False, False, .3, False, [ #ON CONTINUOUSLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_START", False, 100, False), ]
+				), ]
+			),
+			("root", "fluent", "screen_off", .5, False, [ #SCREEN OFF
+				("and", False, False, .33, False, [ #OFF CAUSALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_END", False, 30, False) ]
+				),
+				("and", False, False, .34, False, [ # OFF INERTIALLY - due to the screensaver kicking on (so causally changed because of non-action)
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 100, False),
+					("leaf", "event", "usecomputer_END", False, 1000, False) ]
+				),
+				("and", False, False, .33, False, [ # OFF INERTIALLY - no change because didn't start using the computer
+					("leaf", "prev_fluent", "screen_off", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 200, False) ]
+				) ]
+			)
+		]
+		causal_forest_modified = causal_grammar.generate_causal_forest_from_abbreviated_forest(causal_forest_abbreviated)
+		fluents = {
+			20:  { "screen": 0.9},
+			600: { "screen": 0.8},
+		}
+		actions = {
+			5:  { "usecomputer_END": {"energy": 0, "agent": ("uuid4")} },
+		}
+		xml_string = causal_grammar.process_events_and_fluents(causal_forest_modified, fluents, actions, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = True) # !kDebug: suppress output
+		cls.root = ET.fromstring(xml_string)
+		cls.xml_string = xml_string
+		if kDebug:
+			print(xml_string)
+
+	def testActionOccursOnce(self):
+		action_occurrences = xml_stuff.queryXMLForActionBetweenFrames(self.root,"usecomputer_END",0,1000)
+		assert (action_occurrences == 1), "should have had action usecomputer_END once; n times action occurred: {} [{}]".format(action_occurrences,self.xml_string)
+
+	def testFluentChangeAt20(self):
+		screen_20 = self.root.findall("./fluent_changes/fluent_change[@frame='20']")
+		assert screen_20[0].attrib['new_value'] == 'off', "should have decided screen change to off at 20; was: {}".format(screen_20[0].attrib['new_value'])
+
+	def testTooManyFluentChanges(self):
+		screen_changes = xml_stuff.getFluentChangesForFluentBetweenFrames(self.root, 'screen', 0, 1000)
+		assert (len(screen_changes) == 1), "found {} unexpected fluent changes".format(len(screen_changes)-1)
+
+
+######################## NEW TEST CLASS: Two timeouts for a single action. 2nd fluent is answer: so screensaver turned off computer.
+class MultipleTimeoutsLongerTimeoutWins(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		causal_forest_abbreviated = [ #NOTE: this was yanked from causal_grammar_summerdata.py on 2016-04-24
+			("root", "fluent", "screen_on", .5, False, [ #SCREEN ON
+				("and", False, False, .4, False, [ #ON INERTIALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_END", False, 1, False), ]
+				),
+				("and", False, False, .3, False, [ #ON CONTINUOUSLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_START", False, 100, False), ]
+				), ]
+			),
+			("root", "fluent", "screen_off", .5, False, [ #SCREEN OFF
+				("and", False, False, .33, False, [ #OFF CAUSALLY
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "event", "usecomputer_END", False, 30, False) ]
+				),
+				("and", False, False, .34, False, [ # OFF INERTIALLY - due to the screensaver kicking on (so causally changed because of non-action)
+					("leaf", "prev_fluent", "screen_on", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 100, False),
+					("leaf", "event", "usecomputer_END", False, 1000, False) ]
+				),
+				("and", False, False, .33, False, [ # OFF INERTIALLY - no change because didn't start using the computer
+					("leaf", "prev_fluent", "screen_off", False, False, False),
+					("leaf", "nonevent", "usecomputer_START", False, 200, False) ]
+				) ]
+			)
+		]
+		causal_forest_modified = causal_grammar.generate_causal_forest_from_abbreviated_forest(causal_forest_abbreviated)
+		fluents = {
+			20:  { "screen": 0.8},
+			600: { "screen": 0.9},
+		}
+		actions = {
+			5:  { "usecomputer_END": {"energy": 0, "agent": ("uuid4")} },
+		}
+		xml_string = causal_grammar.process_events_and_fluents(causal_forest_modified, fluents, actions, causal_grammar.kFluentThresholdOnEnergy, causal_grammar.kFluentThresholdOffEnergy, causal_grammar.kReportingThresholdEnergy,not kDebug, handle_overlapping_events = False) # !kDebug: suppress output
+		cls.root = ET.fromstring(xml_string)
+		cls.xml_string = xml_string
+		if kDebug:
+			print(xml_string)
+
+	def testActionOccursOnce(self):
+		action_occurrences = xml_stuff.queryXMLForActionBetweenFrames(self.root,"usecomputer_END",0,1000)
+		assert (action_occurrences == 1), "should have had action usecomputer_END once; n times action occurred: {} [{}]".format(action_occurrences,self.xml_string)
+
+	def testFluentChangeAt600(self):
+		screen_600 = self.root.findall("./fluent_changes/fluent_change[@frame='600']")
+		if (len(screen_600) > 0):
+			assert screen_600[0].attrib['new_value'] == 'off', "should have decided screen changed to off once; was: {}".format(screen_600[0].attrib['new_value'])
+		else:
+			assert 0, "should have decided screen changed to off once; didn't even make it to 600"
+
+	def testTooManyFluentChanges(self):
+		screen_changes = xml_stuff.getFluentChangesForFluentBetweenFrames(self.root, 'screen', 0, 1000)
+		assert (len(screen_changes) == 1), "found {} unexpected fluent changes".format(len(screen_changes) - 1)
+
 
 # TODO: test "nonevent" does what i really want
 # TODO: make sure not averaging to calculate mismatched number of nodes -- need the probabilities to appropriately compensate for that
